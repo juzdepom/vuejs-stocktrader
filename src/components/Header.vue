f165f509015a7bf6ef6934bc4a3d1b946733e9b1
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-02 21:05:16 -0800

    set up firebase vuejs-stocktrader and set up vue-resource

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,8 +29,8 @@
                 Save &amp; Load <span class="caret"></span>
               </a>
               <ul class="dropdown-menu">
-                <li><a href="#">Save Data</a></li>
-                <li><a href="#">Load Data</a></li>
+                <li><a href="#" @click="saveData">Save Data</a></li>
+                <li><a href="#" @click="loadData">Load Data</a></li>
               </ul>
             </li>
           </ul>
@@ -55,7 +55,13 @@ export default {
     // ]),
     endDay(){
       this.$store.dispatch('randomizeStocks')
-    }
+    },
+    saveData(){
+
+    },
+    loadData(){
+
+    },
   },
   computed: {
     funds(){
