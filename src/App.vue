2380ffb43f47f79d9e2f23a6dd199a64606e9ad4
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-02 16:00:59 -0800

    added slide animations

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
 
       <div class="row">
         <div class="col-xs-12">
-          <router-view></router-view>
+          <transition name="slide" mode="out-in">
+            <router-view></router-view>
+          </transition>
         </div>
       </div>
 
@@ -26,5 +28,36 @@
 </script>
 
 <style>
+  .slide-enter {
+    opacity: 0;
+
+  }
+  .slide-enter-active {
+    animation: slide-in 0.3s ease-out forwards;
+    transition: opacity 0.3s;
+  }
+  .slide-leave {
+  }
+  .slide-leave-active {
+    animation: slide-out 0.3s ease-out forwards;
+    transition: opacity 0.3s;
+    opacity: 0;
+  }
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
 
 </style>
