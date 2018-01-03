87b72a9812f0726f87d5733e111a2bf7c2b1acd9
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-02 16:15:16 -0800

    added 'your portfolio is empty'

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <!-- <div class="panel panel-default">
+      <div class="panel-body"> -->
+    <div v-show="isEmpty" class="panel panel-default" style="text-align: center">
+      <h3 style="color: lightgray; margin: 20px"><i>Your Portfolio is Empty.</i></h3>
+    </div>
+
     <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
   </div>
 </template>
@@ -12,7 +18,10 @@
     computed: {
       ...mapGetters({
         stocks: 'stockPortfolio'
-      })
+      }),
+      isEmpty(){
+        return (this.stocks.length == 0)
+      }
     },
     components: {
       appStock: Stock,
